b69f25589e12d875583849377e692e98e7740f62
4l91n170 <nicolasmontoya28@hotmail.com>
2024-05-16 19:18:07 -0500

    logins

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+
+// CONFIGURACION DE GOOGLE
+$clientID = '322873587794-lgauo95vcu7mkfup03k4grn5q19oc0jg.apps.googleusercontent.com';
+$clientSecret = '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';
+$redirectUri = 'http://127.0.0.1/proyecto_git/login.php';
